83cad8df0ecefeca7644c60ad0fcc83a78b2b0c8
Laes Uddin <laesuddin@gmail.com>
2023-01-24 16:26:00 +0600

    Create structure and layout

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import styles from '../style'
+import {discount, robot} from '../assets'
+const Hero = () => (
+  <section id='home' className={`flex md:flex-row flex-col`}>
+
+  </section>
+)
+
+export default Hero
